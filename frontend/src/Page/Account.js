06ad4f1b4f435eac180a8cc5361c41e9939fb016
DaffaMz10 <141113925+DaffaMz10@users.noreply.github.com>
2024-05-02 15:19:12 +0700

    p

---
+++ frontend/src/Page/Account.js
@@ -0,0 +1,48 @@
+// import { Link } from "react-router-dom";
+import React from "react";
+import Edit from "../Asset/edit.png";
+import "../account.css";
+import Logo from "../Asset/logo.png";
+import Back from "../Asset/star.png";
+import Profil from "../Asset/profil.png";
+import Wedit from "../Asset/wedit.png";
+
+
+const Account = () => {
+  return (
+    <div className="container">
+    <div className="cube-account">
+      <img src={Logo} alt="logo" className="logo-account" />
+      <img src={Profil} alt="profil" className="profil-account" /> 
+      <img src={Back} alt="back" className="back" />
+      <img src={Wedit} alt="wedit" className="wedit" />
+      
+      <br />
+      <div className="line"></div>
+
+     
+      <p className="p-account" >Username</p>
+      <input type= "text" name="input" placeholder="Username" />
+      <img src={Edit} alt="edit" className="edit" />
+      
+      <p className="p-account" >Email Adress</p>
+      <input type= "text" name="input" placeholder="Email Adress" />
+      <img src={Edit} alt="edit" className="edit2" />
+      
+      <p className="p-account" >Phone</p>
+      <input type= "text" name="input" placeholder="+62 590 ***" />
+      <img src={Edit} alt="edit" className="edit3" />
+      
+      <p className="p-account" >Password</p>
+      <input type= "text" name="input" placeholder="*************" />
+      <img src={Edit} alt="edit" className="edit4" />
+      <br />
+      <button>Save Change</button>
+      <br />
+      
+      </div>
+      </div>
+  );
+};
+
+export default Account;
