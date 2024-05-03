39aeb02527933f13964384840f23429cb66354db
MaulanaJA92 <maulanajapar92@gmail.com>
2024-05-03 08:54:47 +0700

    p

--- frontend/src/Page/Register.js
+++ frontend/src/Page/Register.js
@@ -1,34 +1,81 @@
-import { Link } from "react-router-dom";
-import React from "react";
-import "../mobile.css"; 
+import { Link, useNavigate } from "react-router-dom";
+import { React, useState } from "react";
+import axios from "axios";
+import "../mobile.css";
 import Logo from "../Asset/logo.png";
 import Back from "../Asset/star.png";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const { username, email, password } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:4000/register", {
+        username,
+        email,
+        password,
+      });
+      navigate("/login");
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="Register-container">
-    <div className="cube">
-      <img src={Logo} alt="Logo" className="logo" />
-      <img src={Back} alt="back" className="back" />
-      <input type="user" name="user" placeholder="Username" />
-      <br />
-      <input type="email" name="Email" placeholder="Email Adress" />
-      <br />
-      <input type="pass" name="pass" placeholder="Password" />
-      <Link to="/" className="forgot-password">Forgot Password?</Link>
-      <br />
-      <button>Register</button>
-      <br />
-      <div className="register-text">
+      <form onSubmit={handleSubmit}>
+        <div className="cube">
+          <img src={Logo} alt="Logo" className="logo" />
+          <img src={Back} alt="back" className="back" />
+          <input
+            type="user"
+            name="username"
+            onChange={handleChange}
+            placeholder="Username"
+          />
+          <br />
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            placeholder="Email Adress"
+          />
+          <br />
+          <input
+            type="pass"
+            name="password"
+            onChange={handleChange}
+            placeholder="Password"
+          />
+          <Link to="/" className="forgot-password">
+            Forgot Password?
+          </Link>
+          <br />
+          <button type="submit">Register</button>
+          <br />
+          <div className="register-text">
             <p>
-              Alredy have an Acount?{" "}
+              Alredy have an Acount?
               <Link to="/Login" className="link-register">
                 Login
               </Link>
             </p>
           </div>
-      </div>
-      </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
