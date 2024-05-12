7834148a38e74e392c6ea5d979f769896dea7926
DaffaMz10 <141113925+DaffaMz10@users.noreply.github.com>
2024-05-12 20:33:46 +0700

    salah cuy

--- frontend/src/Page/Tinderguest.js
+++ frontend/src/Page/Tinderguest.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import '../tinder.css';
+import '../tinderguest.css';
 import Logo from "../Asset/logo.png";
 import Back from "../Asset/star.png";
-import Profil from "../Asset/profil.png";
+import Tutorial from "../Asset/tutorial.png";
 const Tinderguest = () => {
   const [images, setImages] = useState([
     { imageUrl: "https://storage.googleapis.com/rvko-11.appspot.com/foto/gambar1.jpg?GoogleAccessId=firebase-adminsdk-e35xz%40rvko-11.iam.gserviceaccount.com&Expires=16730298000&Signature=drtdRjuv2nAemrhj94ns%2BzrcKBRXRrcfvb70q9uyA89bhsk898CI%2FYJN973ogI903FAAec7RIQ7Zp8sLmPjzpICR8p%2FgCt0n510MU%2FxZ11pen%2BlAGJxUmc7KrE5a9Hgr4tL4KRe8N6qTpyXzAVFQ0YJRStQGLAqbBcZzYKerH0F49xJCVnz9edPdHThACrqnuSnKJEWGlUeRojAKzHEQXcQl%2B2EIsmKoxcJiR%2FWYcj4Zyf%2FZL%2FAf8qT8l%2BoSAhkAdmuZKDAIp%2BlMfStBGBzQQ1qJ5lprAfwBnQTiUpZaezmaoGxDoHJvyMUM%2FjYW4wqB7MUDDJ8xvw1g4n1VqQOU8w%3D%3D", like: 0, dislike: 0 },
@@ -22,8 +22,9 @@ const Tinderguest = () => {
     <div className="container">
       <div className="cube-tinder">
         <img src={Logo} alt="logo" className="logo-account" />
-        <img src={Profil} alt="profil" className="profil-tinder" /> 
+        <img src={Tutorial} alt="tutorial" className="tutorial-tinder" /> 
         <img src={Back} alt="back" className="back-tinder" />
+
         <div className="line"></div>
         <div className="image1" style={{ backgroundImage: `url(${images[currentIndex].imageUrl})` }} onClick={handleSwipeUp}></div>
 
