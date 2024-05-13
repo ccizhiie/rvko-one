e1d94b37ec9d0c0e7f32385e3504d6d7986486aa
DaffaMz10 <141113925+DaffaMz10@users.noreply.github.com>
2024-05-13 14:06:04 +0700

    pvs

--- frontend/src/Page/Language.js
+++ frontend/src/Page/Language.js
@@ -19,27 +19,40 @@ const Language = () => {
           <img src={Back} alt="back" className="back" />
 
           <br />
-          <p className="p-bahasa">Choose Language</p>
+          <p className="p-bahasa">{t("LANGUAGE.p1")}</p>
           <div className="bahasa">
           <img src={Globe} alt="globe" className="globe" />
-          <select className="option">
-            <option value="option1">English</option>
-            <option value="option2">Netherlands</option>
-          </select>
+          <select
+              className="option"
+              onChange={(e) => changeLanguage(e.target.value)}
+            >
+              <option value="en">English</option>
+              <option value="nl">Netherlands</option>
+              <option value="es">Spanyol</option>
+            </select>
+
           </div>
 
           <div className="all-button">
           <br />
           <Link to="/login">
-            <button type="submit" className="purple-button">Login</button>
+          <button type="submit" className="purple-button">
+                {t("LANGUAGE.p2")}
+              </button>
+
             </Link>
             <br/>
             <Link to="/register">
-            <button type="submit" className="grey-button">Register</button>
+            <button type="submit" className="grey-button">
+                {t("LANGUAGE.p3")}
+              </button>
+
             </Link>
             <br/>
             <Link to="/Forgot2">
-            <button type="submit" className="grey-button">Play as Guest</button>
+            <button type="submit" className="grey-button">
+                {t("LANGUAGE.p4")}
+              </button>
             </Link>
           </div>
         </div>
--- frontend/src/Page/Tinderguest.js
+++ frontend/src/Page/Tinderguest.js
@@ -3,6 +3,7 @@ import '../tinderguest.css';
 import Logo from "../Asset/logo.png";
 import Back from "../Asset/star.png";
 import Tutorial from "../Asset/tutorial.png";
+
 const Tinderguest = () => {
   const [images, setImages] = useState([
     { imageUrl: "https://storage.googleapis.com/rvko-11.appspot.com/foto/gambar1.jpg?GoogleAccessId=firebase-adminsdk-e35xz%40rvko-11.iam.gserviceaccount.com&Expires=16730298000&Signature=drtdRjuv2nAemrhj94ns%2BzrcKBRXRrcfvb70q9uyA89bhsk898CI%2FYJN973ogI903FAAec7RIQ7Zp8sLmPjzpICR8p%2FgCt0n510MU%2FxZ11pen%2BlAGJxUmc7KrE5a9Hgr4tL4KRe8N6qTpyXzAVFQ0YJRStQGLAqbBcZzYKerH0F49xJCVnz9edPdHThACrqnuSnKJEWGlUeRojAKzHEQXcQl%2B2EIsmKoxcJiR%2FWYcj4Zyf%2FZL%2FAf8qT8l%2BoSAhkAdmuZKDAIp%2BlMfStBGBzQQ1qJ5lprAfwBnQTiUpZaezmaoGxDoHJvyMUM%2FjYW4wqB7MUDDJ8xvw1g4n1VqQOU8w%3D%3D", like: 0, dislike: 0 },
@@ -10,16 +11,29 @@ const Tinderguest = () => {
     { imageUrl: "https://storage.googleapis.com/rvko-11.appspot.com/foto/gambar3.jpeg?GoogleAccessId=firebase-adminsdk-e35xz%40rvko-11.iam.gserviceaccount.com&Expires=16730298000&Signature=Qdd8KiKEaFoG14VQ%2B2jFyEJeuan9%2FlMvko9QQhSiqOh9lEt8ngNEjWoAcwzGSLXXRsOsBHdbU5%2B5iNEFbIr7QPz0PXDH6ub1nj5GJASFKi3Mu79z86iHC5HS%2BmxXEn8%2Ftsfw1Mf5rrnLXC5BsCJA4fxXvRg8%2F9K%2Fg5t7nNV2sY5v0omrtysl8A8PAU0vTn%2FVBi283BpzA7kwTer%2Fdgvs%2BN2G9DKcHqdhdTPdgsODdoRQydg2VevMqDZIwJO9AkRe0fixhL%2BLisLO8ibFUUn%2FZ%2FFvGiFYK83OvRtMvGJKRBeAGQ5%2B%2BLJIO6TKcwhr%2Bqtx3eRLZTCZ0zLbDvFs8827NA%3D%3D", like: 0, dislike: 0 }
   ]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [showPopup, setShowPopup] = useState(true);
 
   const handleSwipeUp = () => {
-    setCurrentIndex((prevIndex) => {
-      const nextIndex = prevIndex < images.length - 1 ? prevIndex + 1 : 0;
-      return nextIndex;
-    });
+    if (!showPopup) {
+      setCurrentIndex(prevIndex => (prevIndex < images.length - 1 ? prevIndex + 1 : 0));
+    }
+  };
+
+  const handleOutsidePopupClick = (event) => {
+    if (event.target.classList.contains('popup-background')) {
+      setShowPopup(false);
+    }
   };
 
   return (
     <div className="container">
+      {showPopup && (
+        <div className="popup-background" onClick={handleOutsidePopupClick}>
+          <div className="popup">
+            <p>Swipe left to like</p>
+          </div>
+        </div>
+      )}
       <div className="cube-tinder">
         <img src={Logo} alt="logo" className="logo-account" />
         <img src={Tutorial} alt="tutorial" className="tutorial-tinder" /> 
@@ -27,7 +41,6 @@ const Tinderguest = () => {
 
         <div className="line"></div>
         <div className="image1" style={{ backgroundImage: `url(${images[currentIndex].imageUrl})` }} onClick={handleSwipeUp}></div>
-
       </div>
     </div>
   );
