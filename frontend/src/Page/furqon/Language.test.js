c9d12633b9d0d129b7f6ded73611a704ae83098d
DaffaMz10 <141113925+DaffaMz10@users.noreply.github.com>
2024-05-29 12:24:54 +0700

    akhir e cok
    akhir e dadi

--- frontend/src/Page/furqon/Language.test.js
+++ frontend/src/Page/furqon/Language.test.js
@@ -3,19 +3,40 @@ import userEvent from '@testing-library/user-event';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import Language from '../Language';
 import Login from "../Login";
+import Register from "../Register";
+import { click } from '@testing-library/user-event/dist/click';
 
-test('menguji apakah tombol login mengarahkan kita ke halaman login', async () => {
-  render(
-    <MemoryRouter initialEntries={['/']}>
-      <Routes>
-        <Route path="/" element={<Language />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </MemoryRouter>
-  );
+describe('ngetes tolol', () => {
+  it('menguji apakah tombol login mengarahkan kita ke halaman login', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<Language />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </MemoryRouter>
+    );
+  
+    const button = screen.getByRole('button', { name: /LANGUAGE.p3/i });
+      userEvent.click(button);
+    const textada = await screen.findByText(/LOGIN.p5/i);
+    console.log(textada)
+  });
 
-  const button = screen.getByRole('button', { name: /LANGUAGE.p3/i });
-    userEvent.click(button);
-  const textada = await screen.findByText(/LOGIN.p5/i);
-  console.log(textada)
-});
+  it('ngetes goblok', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<Language />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const button = screen.getByRole('button', { name: /LANGUAGE.p4/i });
+      userEvent,click(button);
+    const text = await screen.findByText(/REGISTER.p5/i);
+    console.log(text)
+  })
+})
+
--- frontend/src/daffa/Languagedf.test.js
+++ frontend/src/daffa/Languagedf.test.js
@@ -4,38 +4,39 @@ import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import Language from '../Page/Language';
 import Login from "../Page/Login";
 import Register from "../Page/Register";
+import { click } from '@testing-library/user-event/dist/click';
 
-describe('testing all possible user event in language page', () => {
+describe('melakukan testing apa saja kemungkinan interaksi user di halaman language', () => {
+  it('menguji apakah tombol login mengarahkan kita ke halaman login', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<Language />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </MemoryRouter>
+    );
   
-    it('menguji apakah tombol login mengarahkan kita ke halaman login', async () => {
-        render(
-          <MemoryRouter initialEntries={['/']}>
-            <Routes>
-              <Route path="/" element={<Language />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </MemoryRouter>
-        );
-      
-        const button = screen.getByRole('button', { name: /LANGUAGE.p4/i });
-          userEvent.click(button);
-        await screen.findByText(/LOGIN.p5/i);
-      });
-      
-      it('menguji apakah tombol login mengarahkan kita ke halaman register', async () => {
-          render(
-            <MemoryRouter initialEntries={['/']}>
-              <Routes>
-                <Route path="/" element={<Language />} />
-                <Route path="/register" element={<Register />} />
-              </Routes>
-            </MemoryRouter>
-          );
-        
-          const button = screen.getByRole('button', { name: /LANGUAGE.p3/i });
-            userEvent.click(button);
-await screen.findByText(/REGISTER.p5/i);
-      });
-      
+    const button = screen.getByRole('button', { name: /LANGUAGE.p3/i });
+      userEvent.click(button);
+    const textada = await screen.findByText(/LOGIN.p5/i);
+    console.log(textada)
+  });
+
+  it('menguji apakah tombol register mengarahkan kita ke halaman register', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<Language />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const button = screen.getByRole('button', { name: /LANGUAGE.p4/i });
+      userEvent,click(button);
+    const text = await screen.findByText(/REGISTER.p5/i);
+    console.log(text)
+  })
 })
 
