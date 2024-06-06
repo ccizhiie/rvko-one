7ebfac5df027d4c675ecb27e2784a9ec05984367
MaulanaJA92 <maulanajapar92@gmail.com>
2024-06-06 10:08:44 +0700

    p

---
+++ backend/countfn/counttest.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const path = require('path');
+const acorn = require('acorn');
+const walk = require('acorn-walk');
+
+function countFunctionsInFile(filePath) {
+    try {
+        const code = fs.readFileSync(filePath, 'utf-8');
+        const ast = acorn.parse(code, { ecmaVersion: 'latest', sourceType: 'module' });
+        let functionCount = 0;
+
+        walk.simple(ast, {
+            FunctionDeclaration() {
+                functionCount++;
+            },
+            FunctionExpression() {
+                functionCount++;
+            },
+            ArrowFunctionExpression() {
+                functionCount++;
+            },
+            VariableDeclaration(node) {
+                node.declarations.forEach((declaration) => {
+                    if (declaration.init && (
+                        declaration.init.type === 'FunctionExpression' ||
+                        declaration.init.type === 'ArrowFunctionExpression'
+                    )) {
+                        functionCount++;
+                    }
+                });
+            }
+        });
+
+        return functionCount;
+    } catch (err) {
+        console.error(`Error reading file ${filePath}:`, err);
+        return 0;
+    }
+}
+
+function countFunctionsInDirectory(directoryPath) {
+    let totalFunctions = 0;
+    const fileFunctionCounts = {};
+
+    function walkSync(currentPath) {
+        try {
+            const files = fs.readdirSync(currentPath);
+            files.forEach((file) => {
+                const filePath = path.join(currentPath, file);
+                const stat = fs.statSync(filePath);
+
+                if (stat.isDirectory()) {
+                    walkSync(filePath);
+                } else if (file.endsWith('.js')) {
+                    const functionCount = countFunctionsInFile(filePath);
+                    fileFunctionCounts[filePath] = functionCount;
+                    totalFunctions += functionCount;
+                }
+            });
+        } catch (err) {
+            console.error(`Error reading directory ${currentPath}:`, err);
+        }
+    }
+
+    walkSync(directoryPath);
+    return { totalFunctions, fileFunctionCounts };
+}
+
+// Path to the project directory, relative to the current script file
+const projectDirectory = path.join(__dirname, '../test');
+
+const { totalFunctions, fileFunctionCounts } = countFunctionsInDirectory(projectDirectory);
+
+console.log(`Total number of functions in the project: ${totalFunctions}`);
+console.log('Number of functions in each file:');
+for (const [filePath, count] of Object.entries(fileFunctionCounts)) {
+    console.log(`${filePath}: ${count}`);
+}
---
+++ backend/test.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+
+const app = express();
+const testDir = path.join(__dirname, 'test');
+
+app.use(express.static('public'));
+
+app.get('/files', (req, res) => {
+    fs.readdir(testDir, (err, files) => {
+        if (err) {
+            return res.status(500).send('Unable to scan directory');
+        }
+        res.json(files.filter(file => file.endsWith('.test.js')));
+    });
+});
+
+app.get('/test-functions', (req, res) => {
+    const file = req.query.file;
+    if (!file) {
+        return res.status(400).send('No file specified');
+    }
+    const filePath = path.join(testDir, file);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Unable to read file');
+        }
+        const testFunctions = [];
+        const regex = /it\(['"](.*?)['"],/g;
+        let match;
+        while ((match = regex.exec(data)) !== null) {
+            testFunctions.push(match[1]);
+        }
+        res.json(testFunctions);
+    });
+});
+
+app.get('/run-test', (req, res) => {
+    const file = req.query.file;
+    const func = req.query.func;
+    console.log (func);
+    if (!file || !func) {
+        return res.status(400).send('File or function not specified');
+    }
+    const filePath = path.join( file);
+    const command = `npx jest --forceExit ${filePath} -t "${func}"`;
+    console.log(`Running command: ${command}`);
+    const jestProcess = exec(command, { shell: true });
+
+    let stderr = '';
+    jestProcess.stderr.on('data', (data) => {
+        stderr += data;
+    });
+
+    jestProcess.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+        if (code !== 0) {
+            console.error(`Error: ${stderr}`);
+            return res.status(500).json({ message: 'failed', stderr: stderr });
+        }
+        console.log(`Output: ${stderr}`);
+        res.json({ message: 'success', stderr:stderr });
+    });
+
+});
+
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
